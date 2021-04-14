36b4731da5edfd5f3e2b604957c6ed8c7a1ba66d
Christian Parker <christianparker512@gmail.com>
2021-04-14 18:34:20 -0500

    call apply and bind

--- app.js
+++ app.js
@@ -437,3 +437,57 @@ function tellMeWhenDone(callback){
 tellMeWhenDone(function(){
   alert('I am Done!');
 });
+
+var person = {
+  firstname: 'Christian',
+  lastname: 'parker',
+  getFullName: function() {
+
+    var fullname = this.firstname + ' ' + this.lastname;
+    return fullname;
+
+  }
+}
+
+var logName = function(lang1, lang2) {
+
+  console.log('Logged: ' + this.getFullName());
+  console.log('Arguments: ' + lang1 + ' ' + lang2);
+  console.log('-----------');
+
+}
+
+var logPersonName = logName.bind(person);
+logPersonName('en');
+
+logName.call(person, 'en', 'es');
+logName.apply(person, ['en', 'es']);
+
+(function(lang1, lang2) {
+
+  console.log('Logged: ' + this.getFullName());
+  console.log('Arguments: ' + lang1 + ' ' + lang2);
+  console.log('-----------');
+
+}).apply(person, ['es', 'en']);
+
+// function borrowing
+var person2 = {
+  firstname: 'Lisa',
+  lastname: 'Parker'
+}
+
+console.log(person.getFullName.apply(person2));
+
+// function currying
+function multiply(a, b) {
+  return a*b;
+}
+
+var multipleByTwo = multiply.bind(this, 2);
+console.log(multipleByTwo(4));
+
+var multipleByThree = multiply.bind(this, 3);
+console.log(multipleByThree(4));
+
+
