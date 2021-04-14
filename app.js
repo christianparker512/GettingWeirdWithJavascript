81d2e79a4bffd19a2da0510c09384b52a64b5d17
Christian Parker <christianparker512@gmail.com>
2021-04-14 13:27:24 -0500

    arrays

--- app.js
+++ app.js
@@ -250,10 +250,34 @@ console.log(person["address"]["state"]);
 // a();
 // console.log(newvariable);
 // b();
-var c = {
-  name: 'The c object',
-  log: function(){
-    console.log(this);
-  }
-}
-c.log();
+// var c = {
+//   name: 'The c object',
+//   log: function(){
+//     var self = this;
+//
+//     self.name = 'Updated c object';
+//     console.log(self);
+//
+//     var setname = function(newname){
+//       self.name = newname;
+//     }
+//     setname('Updated again! The c object');
+//     console.log(self);
+//   }
+// }
+// c.log();
+
+var arr = [
+  1,
+  false,
+  {
+    name: 'Christian',
+    address: '46 westerleigh'
+  },
+  function(name){
+  var greeting = 'Hello';
+  console.log(greeting + name);
+  },
+  "Hello"
+];
+console.log(arr);
